caa2ae305ca21d6921338e7384543d7373675813
John Afolayan <jnafolayan@gmail.com>
2019-10-27 16:37:41 +0100

    edit api base url

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,6 +1,8 @@
+const FORCE_PROD = true;
+
 export const buildUrl = (endpoint) => {
 
-  if (process.env.NODE_ENV == 'development')
+  if (!FORCE_PROD && process.env.NODE_ENV == 'development')
     return `http://192.168.4.211:3000/api/v1${endpoint}`
   else
     return `https://parrotbackend.herokuapp.com/api/v1${endpoint}`;
